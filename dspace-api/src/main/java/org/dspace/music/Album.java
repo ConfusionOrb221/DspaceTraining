7cd4a4e99f7748c5193c9b7d1b6a729aaa72bfd0
Nathan Buckingham <nathan.buckingham@atmire.com>
2021-08-27 12:34:01 -0400

    w2p-82793 added Entity Class

---
+++ dspace-api/src/main/java/org/dspace/music/Album.java
@@ -0,0 +1,68 @@
+package org.dspace.music;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "album")
+public class Album {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "artist")
+    private String artist;
+
+    @Column(name = "release_date")
+    private Date releaseDate;
+
+    protected Album(){
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID uuid) {
+        this.id = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
