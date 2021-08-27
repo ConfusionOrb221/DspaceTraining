31f8a1d7f03c898a80c6c65e6cd88f3081661815
Nathan Buckingham <nathan.buckingham@atmire.com>
2021-08-27 15:38:52 -0400

    w2p-82799 created Converter

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/AlbumConverter.java
@@ -0,0 +1,24 @@
+package org.dspace.app.rest.converter;
+
+import org.dspace.app.rest.model.AlbumRest;
+import org.dspace.app.rest.projection.Projection;
+import org.dspace.music.Album;
+
+public class AlbumConverter implements DSpaceConverter<Album, AlbumRest>{
+
+    @Override
+    public AlbumRest convert(Album modelObject, Projection projection) {
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setProjection(projection);
+        albumRest.setId(modelObject.getId());
+        albumRest.setArtist(modelObject.getArtist());
+        albumRest.setTitle(modelObject.getTitle());
+        albumRest.setReleaseDate(modelObject.getReleaseDate());
+        return albumRest;
+    }
+
+    @Override
+    public Class<Album> getModelClass() {
+        return Album.class;
+    }
+}
